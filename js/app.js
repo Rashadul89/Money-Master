329b9cd1c1428d89c571a383b3a137d1808b414c
Rashadul89 <rashadulalam410.com>
2022-07-18 00:55:38 +0600

    food expenses input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
-// console.log('hello-tminol');
+
+// document.getElementById('calculate-btn').addEventListener('click',function(){
+//     // for income calculation 
+//     const incomeInput=document.getElementById('total-income');
+//     const eincomeValueText=incomeInput.value;
+//     const incomeValue=parseFloat(eincomeValueText);
+     
+//    //   get value from Income inputs
+//    const totalIncome=document.getElementById('income-balance');
+//    const totalIncomeText=totalIncome.innerText;
+//    const totalMainIncome=parseFloat(totalIncomeText);
+   
+//    totalIncome.innerText =totalMainIncome+incomeValue;
+//    // clear input field 
+//    incomeInput.value= ' ';
+// });
 document.getElementById('calculate-btn').addEventListener('click',function(){
- const expensesFood=document.getElementById('expenses-food').value;
- const totalExpenses=expensesFood;
+    
+    // for expenses calculation
+ const expensesFoodInput=document.getElementById('expenses-food');
+ const expenceFoodValueText=expensesFoodInput.value;
+ const expenceFoodValue=parseFloat(expenceFoodValueText);
   
+//   get value from food inputs
+const totalExpences=document.getElementById('total-expenses');
+const totalExpencesText=totalExpences.innerText;
+const totalMainExpenses=parseFloat(totalExpencesText);
+
+totalExpences.innerText =totalMainExpenses+expenceFoodValue;
+// clear input field 
+expensesFoodInput.value= ' ';
 });
