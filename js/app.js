d6454e576dfc67b33edb5c7d524d040b62242e7c
Rashadul89 <rashadulalam410.com>
2022-07-17 22:21:39 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// console.log('hello-tminol');
+document.getElementById('calculate-btn').addEventListener('click',function(){
+ const expensesFood=document.getElementById('expenses-food').value;
+ const totalExpenses=expensesFood;
+  
+});
