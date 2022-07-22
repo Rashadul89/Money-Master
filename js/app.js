dd74ec4a461e2227368c6b14dae3ac259db46307
Rashadul89 <rashadulalam410.com>
2022-07-22 10:33:53 +0600

    adding all condition

--- js/app.js
+++ js/app.js
@@ -12,46 +12,39 @@ function updateValue(incomeValue, expensesFoodValue, expensesRentValue, expenses
     const allExpensesValue = expensesFoodValue + expensesRentValue + expensesClothesValue;
     const allTotalExpenses = totalMainExpenses + allExpensesValue;
     totalExpenses.innerText = allTotalExpenses;
-
     const totalExpensesIncome = document.getElementById('balanceAfterExpenses');
     const totalMainBalance = incomeValue - allExpensesValue
     totalExpensesIncome.innerText = totalMainBalance;
 };
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const incomeValue = getInputValue('income-filed');
-    const expensesFoodValue = getInputValue('expenses-food');
-    const expensesRentValue = getInputValue('expenses-rent');
-    const expensesClothesValue = getInputValue('expenses-clothes');
-
-    updateValue(incomeValue, expensesFoodValue, expensesRentValue, expensesClothesValue);
-
+    if(incomeValue>0){
+        const expensesFoodValue = getInputValue('expenses-food');
+        const expensesRentValue = getInputValue('expenses-rent');
+        const expensesClothesValue = getInputValue('expenses-clothes');
+        updateValue(incomeValue, expensesFoodValue, expensesRentValue, expensesClothesValue);
+    };
+ 
+   
 });
-function takePreValue(){
-    const totalExpenses = document.getElementById('total-expenses');
-    const expensestext = totalExpenses.innerText;
-    const expenses = parseFloat(expensestext);
-    return expenses;
-}
+function takePreValue(takeTextValue) {
+    const totalNewAmount = document.getElementById(takeTextValue);
+    const expensestext = totalNewAmount.innerText;
+    const totalCost = parseFloat(expensestext);
+    return totalCost;
+};
 document.getElementById('save-balanceBtn').addEventListener('click', function () {
-    // const totalExpenses = document.getElementById('total-expenses');
-    // const expensestext = totalExpenses.innerText;
-    // const expenses = parseFloat(expensestext);
-    takePreValue();
-
-    const totalExpensesIncome = document.getElementById('balanceAfterExpenses');
-    const findTotalBalanceText = totalExpensesIncome.innerText;
-    const balance = parseFloat(findTotalBalanceText);
-
+    const expenses = takePreValue('total-expenses');
+    const balance = takePreValue('balanceAfterExpenses');
     const addingBalance = expenses + balance;
-
     const saveSomeMoney = getInputValue('salaryFiledSaveInput');
+    if(saveSomeMoney>0){
+        const saveCalculate = (addingBalance / 100) * saveSomeMoney;
+        const preSaveAmount = document.getElementById('saving-amount');
+        preSaveAmount.innerText = saveCalculate;
+        const remainingAmount = document.getElementById('remaining-amount');
+        const setRemaining = balance - saveCalculate;
+        remainingAmount.innerText = setRemaining;
+    };
 
-    const saveCalculate = (addingBalance / 100) * saveSomeMoney;
-
-    const preSaveAmount = document.getElementById('saving-amount');
-    preSaveAmount.innerText = saveCalculate;
-
-    const remainingAmount = document.getElementById('remaining-amount');
-    const setRemaining= balance-saveCalculate;
-    remainingAmount.innerText=setRemaining;
 });
